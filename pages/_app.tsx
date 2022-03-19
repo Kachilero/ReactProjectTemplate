cf1b5c223e1fd503781dcd75eebcb0d7102641f5
Alejandro Cerro <alejcerro@gmail.com>
2022-03-19 13:38:13 -0500

    Added global styles wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,38 @@
-import "../styles/globals.css";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const GlobalStyles = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070F3'
+  }
 }
 
-export default MyApp;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App;
